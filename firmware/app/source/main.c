1d6912a0661a8c3d4c5dfa31e331dd598bc90d8e
rksdna <rksdna@mail.ru>
2017-12-03 19:24:52 +0400

    core

--- firmware/app/source/main.c
+++ firmware/app/source/main.c
@@ -4,6 +4,7 @@
 #include <stm32/gpio.h>
 #include <stm32/rcc.h>
 #include <stm32/des.h>
+#include <stm32/spi.h>
 #include <threads.h>
 #include <timers.h>
 #include <debug.h>
@@ -21,52 +22,57 @@ struct stream debug_stream = {debug_put, 0};
 
 void startup_board(void)
 {
-   FLASH->ACR = FLASH_ACR_LATENCY_48MHz | FLASH_ACR_PRFTBE;
-
-   RCC->CR = RCC_CR_HSEON | RCC_CR_HSION;
-   wait_for(&RCC->CR, RCC_CR_HSERDY, RCC_CR_HSERDY);
-
-   RCC->CFGR = RCC_CFGR_PLLMUL12 | RCC_CFGR_PLLXTPRE | RCC_CFGR_PLLSRC_HSE_PREDIV;
-   RCC->CR = RCC_CR_HSEON | RCC_CR_HSION | RCC_CR_PLLON;
-   wait_for(&RCC->CR, RCC_CR_PLLRDY, RCC_CR_PLLRDY);
-
-   RCC->CFGR = RCC_CFGR_PLLMUL12 | RCC_CFGR_PLLXTPRE | RCC_CFGR_PLLSRC_HSE_PREDIV | RCC_CFGR_SW_PLL;
-   RCC->CFGR3 = 0;
-
    RCC->AHBENR = RCC_AHBENR_SRAMEN | RCC_AHBENR_FLITFEN | RCC_AHBENR_GPIOAEN;
-   RCC->APB1ENR = RCC_APB1ENR_USART2EN | RCC_APB1ENR_USBEN;
-   RCC->APB2ENR =  RCC_APB2ENR_SYSCFGCOMPEN;
+   RCC->APB1ENR = RCC_APB1ENR_USART2EN;
+   RCC->APB2ENR = RCC_APB2ENR_SYSCFGCOMPEN | RCC_APB2ENR_SPI1EN;
 
    SYSCFG->CFGR1 = SYSCFG_CFGR1_PA11_PA12_RMP;
 
-   GPIOA->MODER = GPIO_MODER_MODER0_0 | GPIO_MODER_MODER14_1;
-   GPIOA->PUPDR = 0;
-
-   GPIOA->AFRH = (1 << 6 * 4);
+   GPIOA->ODR = GPIO_ODR_ODR7;
+   GPIOA->MODER =
+           /* CPL_NSS */ GPIO_MODER_GPO(4) |
+           /* CPL_SCK */ GPIO_MODER_AFO(5) |
+           /* CPL_SDI */ GPIO_MODER_AFO(6) |
+           /* CPL_SDO */ GPIO_MODER_AFO(7) |
+           /* SYS_TXD */ GPIO_MODER_AFO(14) |
+           /* SYS_RXD */ GPIO_MODER_GPI(15);
+   GPIOA->AFRL = GPIO_AFRL(4, 0) | GPIO_AFRL(5, 0) | GPIO_AFRL(6, 0);
+   GPIOA->AFRH = GPIO_AFRH(14, 1);
 
    USART2->CR1 = USART_CR1_UE | USART_CR1_TE;
    USART2->CR2 = 0;
-   USART2->BRR = 48000000 / 115200 + 1;
+   USART2->BRR = 8000000 / 115200 + 1;
 
-   start_timers_clock(48000);
+   SPI1->CR2 = SPI_CR2_FRXTH | SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2;
+   SPI1->CR1 = SPI_CR1_MSTR | SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_SPE | SPI_CR1_BR;
+
+   start_timers_clock(8000);
 
    debug("hello\n");
+   debug("id: %*m flash: %dKbytes\n", sizeof(DES->ID), DES->ID, DES->FSIZE & DES_FSIZE_FSIZE);
 }
 
-void board_info(void)
+#define SPI1_DR_8 *(volatile u8_t *)(&SPI1->DR)
+
+u8_t spi_poll(u8_t value)
 {
-   debug("id: %*m flash: %dKbytes\n", sizeof(DES->ID), DES->ID, DES->FSIZE & DES_FSIZE_FSIZE);
+    SPI1_DR_8 = value;
+    wait_for(&SPI1->SR, SPI_SR_RXNE, SPI_SR_RXNE);
+    return SPI1_DR_8;
 }
 
 void main(void)
 {
     startup_board();
-    board_info();
 
     while (1)
     {
-        sleep(1000);
+        u8_t s;
+        GPIOA->BSRR = GPIO_BSRR_BR4;
+        s = spi_poll(0xA5);
+        GPIOA->BSRR = GPIO_BSRR_BS4;
+
+        debug(" -> %x\n", s);
+        sleep(50);
     }
-
-
 }
